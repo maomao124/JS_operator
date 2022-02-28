fbaa50a7239e28a6628ea5d303ece29642ffdebf
毛 <1296193245@qq.com>
2022-02-28 17:41:57 +0800

    JS_operator

---
+++ src/main/webapp/test6.js
@@ -0,0 +1,21 @@
+/**
+ * Project name(项目名称)：JS_operator
+ * File name(文件名): test6
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/2/28
+ * Time(创建时间)： 17:40
+ * Version(版本): 1.0
+ * Description(描述)：
+ * 逻辑运算符
+ 逻辑运算符通常用来组合多个表达式，逻辑运算符的运算结果是一个布尔值，只能有两种结果，不是 true 就是 false。
+ */
+
+
+
+// 运算符	名称	示例
+// &&	逻辑与	x && y 表示如果 x 和 y 都为真，则为真
+// ||	逻辑或	x || y 表示如果 x 或 y 有一个为真，则为真
+// !	逻辑非	!x 表示如果 x 不为真，则为真
+
